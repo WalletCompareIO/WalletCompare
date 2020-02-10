fb55e7b900aec9747d91c5f7b142d294bba78911
Hammad Jutt <jutt@ualberta.ca>
2020-02-10 00:41:09 -0700

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+const siteMetadata = {
+  name: 'WalletCompare',
+  description: 'Discover and compare Ethereum wallets',
+};
+
+module.exports = {
+  siteMetadata,
+  plugins: [
+    'gatsby-plugin-chakra-ui',
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        ...siteMetadata,
+        display: 'minimal-ui',
+        theme_color: '#5035be',
+        background_color: 'white',
+        icon: 'src/assets/favicon.png',
+        lang: 'en-US',
+        start_url: '/',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-typescript',
+  ],
+};
